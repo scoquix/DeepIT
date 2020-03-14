307dcdbabd24336ec32ff0cd45677c51ce99d385
scoquix <sebaszafranski@gmail.com>
2020-03-14 23:03:30 +0100

    initial commit

---
+++ src/main/java/com/example/deepit/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.deepit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    String hello() {
+        return "Hello World";
+    }
+
+}
---
+++ src/test/java/com/example/deepit/DeepitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deepit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
