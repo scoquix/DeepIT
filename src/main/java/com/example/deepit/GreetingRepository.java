251e7ec3b7b3382c5de05a064d95ede59f8ae8bf
scoquix <sebaszafranski@gmail.com>
2020-03-14 23:26:33 +0100

    add db

---
+++ src/main/java/com/example/deepit/GreetingRepository.java
@@ -0,0 +1,6 @@
+package com.example.deepit;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GreetingRepository extends CrudRepository<Greeting,Long>{
+}
--- src/main/java/com/example/deepit/HelloController.java
+++ src/main/java/com/example/deepit/HelloController.java
@@ -5,9 +5,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private final GreetingRepository repository;
+
+    public HelloController(GreetingRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/")
     String hello() {
         return "Hello World";
     }
 
+    @GetMapping("/greeting")
+    Iterable<Greeting> greetings() {
+        return repository.findAll();
+    }
 }
