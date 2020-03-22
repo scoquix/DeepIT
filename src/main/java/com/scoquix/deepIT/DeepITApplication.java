acd3d42dd520db99353397fd9727329b5636f95a
scoquix <sebaszafranski@gmail.com>
2020-03-22 18:23:05 +0100

    Renaming Changes

--- src/main/java/com/scoquix/deepIT/DeepITApplication.java
+++ src/main/java/com/scoquix/deepIT/DeepITApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringdataApplication {
+public class DeepITApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringdataApplication.class, args);
+        SpringApplication.run(DeepITApplication.class, args);
     }
 
 }
