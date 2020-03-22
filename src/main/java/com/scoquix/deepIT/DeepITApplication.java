1a1614f1b2df1784e841b45a927c912bdd088146
scoquix <sebaszafranski@gmail.com>
2020-03-22 19:13:53 +0100

    Small Renaming changes

--- src/main/java/com/scoquix/deepIT/DeepITApplication.java
+++ src/main/java/com/scoquix/deepIT/DeepITApplication.java
@@ -1,13 +1,27 @@
 package com.scoquix.deepIT;
 
+import com.scoquix.deepIT.model.User;
+import com.scoquix.deepIT.repository.UserRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DeepITApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(DeepITApplication.class, args);
+
+
     }
+    @Bean
+    ApplicationRunner applicationRunner(UserRepository repository){
+		return args -> {
+			repository.save(new User());
+		};
+	}
 
 }
