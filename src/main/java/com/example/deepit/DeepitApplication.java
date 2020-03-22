cf8d9f65ecd089045d4154b48bfb24563a79fdeb
scoquix <sebaszafranski@gmail.com>
2020-03-22 18:48:52 +0100

    Small Renaming changes

--- src/main/java/com/example/deepit/DeepitApplication.java
+++ src/main/java/com/example/deepit/DeepitApplication.java
@@ -1,21 +1,16 @@
 package com.example.deepit;
 
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-@SpringBootApplication
-public class DeepitApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DeepitApplication.class, args);
-	}
-
-	@Bean
-	ApplicationRunner applicationRunner(GreetingRepository repository){
-		return args -> {
-			repository.save(new Greeting("hello"));
-			repository.save(new Greeting("hi"));
-		};
-	}
-}
+//@SpringBootApplication
+//public class DeepitApplication {
+//	public static void main(String[] args) {
+//		SpringApplication.run(DeepitApplication.class, args);
+//	}
+//
+//	@Bean
+//	ApplicationRunner applicationRunner(GreetingRepository repository){
+//		return args -> {
+//			repository.save(new Greeting("hello"));
+//			repository.save(new Greeting("hi"));
+//		};
+//	}
+//}
--- src/main/java/com/example/deepit/Greeting.java
+++ src/main/java/com/example/deepit/Greeting.java
@@ -1,30 +1,30 @@
-package com.example.deepit;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-@Entity
-public class Greeting {
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    @Column
-    private String message;
-
-    public Greeting() {}
-
-    public Greeting(String message){
-        this.message = message;
-    }
-
-    public Long getId(){
-        return id;
-    }
-
-    public String getMessage(){
-        return message;
-    }
-}
+//package com.example.deepit;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+//
+//@Entity
+//public class Greeting {
+//    @Id
+//    @GeneratedValue
+//    private Long id;
+//
+//    @Column
+//    private String message;
+//
+//    public Greeting() {}
+//
+//    public Greeting(String message){
+//        this.message = message;
+//    }
+//
+//    public Long getId(){
+//        return id;
+//    }
+//
+//    public String getMessage(){
+//        return message;
+//    }
+//}
