7b04a330ec13aa9880d4e848b38b872862dce913
scoquix <sebaszafranski@gmail.com>
2020-04-09 00:54:20 +0200

    NEW docs class with API documentation info

--- src/main/java/com/scoquix/deepIT/controller/DocsController.java
+++ src/main/java/com/scoquix/deepIT/controller/DocsController.java
@@ -12,15 +12,15 @@ public class DocsController {
     public String home(){
         StringBuilder builder = new StringBuilder();
         builder.append("Witaj w Dokumentacji ! ");
-        builder.append("\n\n");
+        builder.append("<br><br>");
         builder.append("Articles Endpointy: ");
         builder.append(" /api/articles/all   <- (GET) zwraca wszystkie artykuły");
         builder.append(" /api/articles/1   <- (GET) zwraca pierwszy artykuł");
         builder.append(" /api/articles/   <- (POST) dodaje artykuł, nalezy w request body przeslac JSON-a");
-        builder.append("np. \n{ \n\t\"author_id\":\"12\",\n\t\"content\":\"12\",\n\t\"title\":\"12\"\n}");
+        builder.append("np. <br>{ <br>&#09;\"author_id\":\"12\",<br>&#09;\"content\":\"12\",<br>&#09;\"title\":\"12\"<br>}");
         builder.append(" /api/articles/ <- (PUT) aktualizuje artykuł, nalezy w request body przeslac JSON-a ( jak wyzej ale dodajac article_id)");
         builder.append(" /api/articles?index=1 <- (DELETE) usuwa artykuł, o article_id = 1");
-        builder.append("\n Zapytania POST, PUT i DELETE będą zwracać HttpStatus np. POST -> CREATED, PUT -> ACCEPTED, DELETE -> NO_CONTENT");
+        builder.append("<br> Zapytania POST, PUT i DELETE będą zwracać HttpStatus np. POST -> CREATED, PUT -> ACCEPTED, DELETE -> NO_CONTENT");
 
         return builder.toString();
     }
