6851f8eac12062e7261c02654acc1857425b1bf0
scoquix <sebaszafranski@gmail.com>
2020-04-09 00:42:45 +0200

    NEW docs class with API documentation info

---
+++ src/main/java/com/scoquix/deepIT/controller/DocsController.java
@@ -0,0 +1,28 @@
+package com.scoquix.deepIT.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/docs")
+@RestController
+public class DocsController {
+    @GetMapping
+    public String home(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("Witaj w Dokumentacji ! ");
+        builder.append("\n\n");
+        builder.append("Articles Endpointy: ");
+        builder.append(" /api/articles/all   <- (GET) zwraca wszystkie artykuły");
+        builder.append(" /api/articles/1   <- (GET) zwraca pierwszy artykuł");
+        builder.append(" /api/articles/   <- (POST) dodaje artykuł, nalezy w request body przeslac JSON-a");
+        builder.append("np. { \"author_id\":\"12\",\n \t\"content\":\"12\",\n \t\"title\":\"12\"");
+        builder.append(" /api/articles/ <- (PUT) aktualizuje artykuł, nalezy w request body przeslac JSON-a ( jak wyzej ale dodajac article_id)");
+        builder.append(" /api/articles?index=1 <- (DELETE) usuwa artykuł, o article_id = 1");
+        builder.append("\n Zapytania POST, PUT i DELETE będą zwracać HttpStatus np. POST -> CREATED, PUT -> ACCEPTED, DELETE -> NO_CONTENT");
+
+        return builder.toString();
+    }
+
+}
