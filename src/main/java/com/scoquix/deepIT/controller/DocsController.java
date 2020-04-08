b6d2dc70687551932f622d97572b05f27d3c5719
scoquix <sebaszafranski@gmail.com>
2020-04-09 00:47:20 +0200

    NEW docs class with API documentation info

--- src/main/java/com/scoquix/deepIT/controller/DocsController.java
+++ src/main/java/com/scoquix/deepIT/controller/DocsController.java
@@ -17,7 +17,7 @@ public class DocsController {
         builder.append(" /api/articles/all   <- (GET) zwraca wszystkie artykuły");
         builder.append(" /api/articles/1   <- (GET) zwraca pierwszy artykuł");
         builder.append(" /api/articles/   <- (POST) dodaje artykuł, nalezy w request body przeslac JSON-a");
-        builder.append("np. { \"author_id\":\"12\",\n \t\"content\":\"12\",\n \t\"title\":\"12\"");
+        builder.append("np. \n{ \n\t\"author_id\":\"12\",\n\t\"content\":\"12\",\n\t\"title\":\"12\"\n}");
         builder.append(" /api/articles/ <- (PUT) aktualizuje artykuł, nalezy w request body przeslac JSON-a ( jak wyzej ale dodajac article_id)");
         builder.append(" /api/articles?index=1 <- (DELETE) usuwa artykuł, o article_id = 1");
         builder.append("\n Zapytania POST, PUT i DELETE będą zwracać HttpStatus np. POST -> CREATED, PUT -> ACCEPTED, DELETE -> NO_CONTENT");
