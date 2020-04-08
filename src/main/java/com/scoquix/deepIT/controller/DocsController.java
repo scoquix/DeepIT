66293567bb3ead5e0ea613ae088e97f0b90fa55f
scoquix <sebaszafranski@gmail.com>
2020-04-09 01:06:48 +0200

    NEW docs class with API documentation info

--- src/main/java/com/scoquix/deepIT/controller/DocsController.java
+++ src/main/java/com/scoquix/deepIT/controller/DocsController.java
@@ -11,16 +11,16 @@ public class DocsController {
     @GetMapping
     public String home(){
         StringBuilder builder = new StringBuilder();
-        builder.append("Witaj w Dokumentacji ! ");
+        builder.append("<p>Witaj w Dokumentacji ! </p>");
         builder.append("<br><br>");
-        builder.append("Articles Endpointy: ");
-        builder.append(" /api/articles/all   <- (GET) zwraca wszystkie artykuły");
-        builder.append(" /api/articles/1   <- (GET) zwraca pierwszy artykuł");
-        builder.append(" /api/articles/   <- (POST) dodaje artykuł, nalezy w request body przeslac JSON-a");
-        builder.append("np. <br>{ <br>&#09;\"author_id\":\"12\",<br>&#09;\"content\":\"12\",<br>&#09;\"title\":\"12\"<br>}");
-        builder.append(" /api/articles/ <- (PUT) aktualizuje artykuł, nalezy w request body przeslac JSON-a ( jak wyzej ale dodajac article_id)");
-        builder.append(" /api/articles?index=1 <- (DELETE) usuwa artykuł, o article_id = 1");
-        builder.append("<br> Zapytania POST, PUT i DELETE będą zwracać HttpStatus np. POST -> CREATED, PUT -> ACCEPTED, DELETE -> NO_CONTENT");
+        builder.append("<p>Articles Endpointy: </p>");
+        builder.append("<p> /api/articles/all   <- (GET) zwraca wszystkie artykuły</p>");
+        builder.append("<p> /api/articles/1   <- (GET) zwraca pierwszy artykuł</p>");
+        builder.append("<p> /api/articles/   <- (POST) dodaje artykuł, nalezy w request body przeslac JSON-a</p>");
+        builder.append("<p>np. <br>{ <br>&#09;\"author_id\":\"12\",<br>&#09;\"content\":\"12\",<br>&#09;\"title\":\"12\"<br>}</p>");
+        builder.append("<p> /api/articles/ <- (PUT) aktualizuje artykuł, nalezy w request body przeslac JSON-a ( jak wyzej ale dodajac article_id)</p>");
+        builder.append("<p> /api/articles?index=1 <- (DELETE) usuwa artykuł, o article_id = 1</p>");
+        builder.append("<br><p> Zapytania POST, PUT i DELETE będą zwracać HttpStatus np. POST -> CREATED, PUT -> ACCEPTED, DELETE -> NO_CONTENT</p>");
 
         return builder.toString();
     }
