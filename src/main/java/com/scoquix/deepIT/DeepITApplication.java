00736f85e8c9070c25e5f00d8f92988a361f578a
scoquix <sebaszafranski@gmail.com>
2020-04-30 12:20:14 +0200

    Global Cross Origin AllowAll

--- src/main/java/com/scoquix/deepIT/DeepITApplication.java
+++ src/main/java/com/scoquix/deepIT/DeepITApplication.java
@@ -2,9 +2,6 @@ package com.scoquix.deepIT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DeepITApplication {
@@ -13,17 +10,4 @@ public class DeepITApplication {
         SpringApplication.run(DeepITApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api")
-                        .allowedOrigins("*")
-                        .allowedHeaders("*")
-                        .allowedMethods("*")
-                        .maxAge(3600);
-            }
-        };
-    }
 }
